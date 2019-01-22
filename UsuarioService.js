67df318feae64ca6ac9ccd8fd3f9e9368673aa75
Rony Moura <ronymmoura@gmail.com>
2019-01-22 18:16:21 -0200

    Teste de novo service

--- UsuarioService.js
+++ UsuarioService.js
@@ -1,12 +1,11 @@
 import BaseService from "@intechprev/service";
 
 export default class UsuarioService extends BaseService {
-    Buscar() {
-        return this.CriarRequisicao("GET", "/usuario");
+    Buscar(versao = null) {
+        return this.CriarRequisicao("GET", this.MontarRota("usuario", versao));
     }
 
-    Login(cpf, senha) {
-        return this.CriarRequisicao("POST", "/usuario/login", { Cpf: cpf, Senha: senha });
+    Login(cpf, senha, versao = null) {
+        return this.CriarRequisicao("POST", this.MontarRota("usuario", versao, "login"), { Cpf: cpf, Senha: senha });
     }
-
 }
